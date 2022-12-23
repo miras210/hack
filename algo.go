5966d38d588533d46e5f4d89a3528a895e7f8b9d
Beknur <rbeknur@wtotem.net>
2022-12-24 00:35:23 +0600

    back to working version

--- algo.go
+++ algo.go
@@ -1,8 +1,6 @@
 package main
 
-import (
-	"math"
-)
+import "math"
 
 type Bag struct {
 	Gifts  []Gift
@@ -12,19 +10,16 @@ type Bag struct {
 
 func (b *Bag) Result() []int {
 	res := make([]int, len(b.Gifts))
-
 	for i := 0; i < len(b.Gifts); i++ {
 		res[i] = b.Gifts[i].ID
 	}
-
 	return res
 }
-
 func (b *Bag) Add(gift Gift) bool {
-	if b.Weight+gift.Weight >= 200 {
+	if b.Weight+gift.Weight > 200 {
 		return false
 	}
-	if b.Volume+gift.Volume >= 100 {
+	if b.Volume+gift.Volume > 100 {
 		return false
 	}
 	b.Gifts = append(b.Gifts, gift)
@@ -32,18 +27,16 @@ func (b *Bag) Add(gift Gift) bool {
 	b.Volume += gift.Volume
 	return true
 }
-
 func (b *Bag) AddMax(gifts []Gift) int {
-	for i := 0; i < len(gifts); i++ {
+	for i := len(gifts) - 1; i >= 0; i-- {
 		if !b.Add(gifts[i]) {
-			return i
+			return i + 1
 		}
 	}
-
-	return len(gifts)
+	return 0
 }
 
-const bagSize = 10
+const bagSize = 15
 
 func Algo(children []Coords, gifts []Gift) Request {
 	res := Request{
@@ -52,19 +45,19 @@ func Algo(children []Coords, gifts []Gift) Request {
 		StackOfBags: make([][]int, 0),
 	}
 
-	for len(gifts) != 0 {
+	for i := 0; i < len(gifts); i += bagSize {
 		currx, curry := 0, 0
 		bag := Bag{
 			Gifts:  make([]Gift, 0),
 			Weight: 0,
 			Volume: 0,
 		}
-		i := bag.AddMax(gifts)
-		gifts = gifts[i:]
+		for j := i; j < i+bagSize && j < len(gifts); j++ {
+			bag.Add(gifts[j])
+		}
 		res.StackOfBags = append(res.StackOfBags, bag.Result())
 
 		count := len(bag.Gifts)
-
 		for count != 0 {
 			idx := Closest(children, currx, curry)
 			currx, curry = children[idx].X, children[idx].Y
@@ -73,7 +66,6 @@ func Algo(children []Coords, gifts []Gift) Request {
 			children = children[:len(children)-1]
 			count--
 		}
-
 		if len(gifts) != 0 {
 			zero := Coords{
 				X: 0,
@@ -82,15 +74,12 @@ func Algo(children []Coords, gifts []Gift) Request {
 			res.Moves = append(res.Moves, zero)
 		}
 	}
-
 	return res
 }
-
 func Closest(children []Coords, x, y int) int {
 	cx, cy := children[0].X, children[0].Y
 	dist := Distance(cx, cy, x, y)
 	ans := 0
-
 	for i := 1; i < len(children); i++ {
 		cx, cy = children[i].X, children[i].Y
 		newDist := Distance(cx, cy, x, y)
@@ -99,10 +88,8 @@ func Closest(children []Coords, x, y int) int {
 			ans = i
 		}
 	}
-
 	return ans
 }
-
 func Distance(x1, y1, x2, y2 int) float64 {
 	return math.Sqrt(math.Pow(float64(x2-x1), 2) + math.Pow(float64(y2-y1), 2))
 }
